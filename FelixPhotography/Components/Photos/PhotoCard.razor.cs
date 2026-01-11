1f2cfa7943b6211410e34053ce280ecf0f35b2d9
Felix-Silva <felixsilva316@outlook.com>
2026-01-10 22:28:46 -0600

    initial commit

---
+++ FelixPhotography/Components/Photos/PhotoCard.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FelixPhotography.Components.Photos;
+
+public partial class PhotoCard
+{
+    [Parameter, EditorRequired]
+    public string Src { get; set; } = string.Empty;
+}
